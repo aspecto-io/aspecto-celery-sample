c783326f43ffaf288cfb66d239b550c862142f7d
Tom Z <tom@aspecto.io>
2022-03-22 14:51:22 +0200

    Change to batch span processor

--- tracing.py
+++ tracing.py
@@ -2,7 +2,7 @@ from opentelemetry.instrumentation.celery import CeleryInstrumentor
 # from opentelemetry.instrumentation.redis import RedisInstrumentor
 
 import logging
-bootstrap_logger = logging.getLogger('bootstrap')
+bootstrap_logger = logging.getLogger('opentelemetry')
 bootstrap_logger.setLevel(logging.DEBUG)
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
@@ -17,7 +17,7 @@ from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExport
 def instrument(*args, **kwargs):
     provider = TracerProvider()
     # simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-    simple_processor = SimpleSpanProcessor(OTLPSpanExporter())
+    simple_processor = BatchSpanProcessor(OTLPSpanExporter())
     provider.add_span_processor(simple_processor)
     trace.set_tracer_provider(provider)
     tracer = trace.get_tracer(__name__)
